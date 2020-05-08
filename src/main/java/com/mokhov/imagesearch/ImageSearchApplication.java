e03a180c9818949e5855aa7ef4d2b49b71a8a792
mikhail.mokhov <mikhail.mokhov@caseware.com>
2020-05-08 18:13:28 -0400

    Initial commit

---
+++ src/main/java/com/mokhov/imagesearch/ImageSearchApplication.java
@@ -0,0 +1,13 @@
+package com.mokhov.imagesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageSearchApplication.class, args);
+    }
+
+}
