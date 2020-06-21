35da93be5f3eb9448ef077a01b92131522bba307
mikhail.mokhov <mikhail.mokhov@caseware.com>
2020-06-20 22:15:16 -0400

    Added (EFK) Logging Stack

--- src/main/java/com/mokhov/climbing/ClimbingApplication.java
+++ src/main/java/com/mokhov/climbing/ClimbingApplication.java
@@ -1,19 +1,18 @@
 package com.mokhov.climbing;
 
+import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClimbingApplication {
-
     public static void main(String[] args) {
-//        nu.pattern.OpenCV.loadShared();
-//        System.out.println(Core.VERSION);
-//        System.out.println(Core.VERSION_MAJOR);
-//        System.out.println(Core.VERSION_REVISION);
-//        System.out.println(Core.NATIVE_LIBRARY_NAME);
-//        System.out.println(Core.getBuildInformation());
+        nu.pattern.OpenCV.loadShared();
+        System.out.println(Core.VERSION);
+        System.out.println(Core.VERSION_MAJOR);
+        System.out.println(Core.VERSION_REVISION);
+        System.out.println(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.getBuildInformation());
         SpringApplication.run(ClimbingApplication.class, args);
     }
-
 }
